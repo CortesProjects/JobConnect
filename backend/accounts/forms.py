5428e3e8d5dca84483c65aca73b26b5061427e30
Ken <kencortes45@gmail.com>
2025-09-29 22:33:47 +0800

    setup(accounts): setup employer login and register

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-from .models import StudentProfile
+from .models import StudentProfile, EmployerProfile
 
 User = get_user_model()
 
@@ -62,4 +62,58 @@ class StudentRegistrationForm(UserCreationForm):
             user.save()
             # StudentProfile.objects.create(user=user)
             
+        return user
+
+
+class EmployerRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Business Email")
+    company_name = forms.CharField(max_length=255, required=True, label="Company Name")
+    industry = forms.CharField(max_length=255, required=True, label="Industry")
+    terms_and_conditions = forms.BooleanField(
+        required=True,
+        label="I've read and agree with your Terms of Service"
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = (
+            'email',
+            'company_name',
+            'industry'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if 'username' in self.fields:
+            del self.fields['username']  # remove username like student form
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+
+            if name == 'email':
+                field.widget.attrs['placeholder'] = 'Business Email'
+            elif name == 'company_name':
+                field.widget.attrs['placeholder'] = 'Company Name'
+            elif name == 'industry':
+                field.widget.attrs['placeholder'] = 'Industry'
+            elif name == 'password1':
+                field.widget.attrs['placeholder'] = 'Password'
+                field.label = False
+            elif name == 'password2':
+                field.widget.attrs['placeholder'] = 'Confirm Password'
+                field.label = False
+
+            if name == 'terms_and_conditions':
+                field.widget.attrs.pop('class', None)
+                field.label = False
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.user_type = 'employer'
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+            EmployerProfile.objects.create(user=user)
         return user
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import employer_register, employer_login
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('registration/', views.student_register, name='register'),
     # path('login/', views.LoginView.as_view(), name='login'),
+    path('employer/register/', employer_register, name='employer_register'),
+    path('employer/login/', employer_login, name='employer_login'),
 ]
